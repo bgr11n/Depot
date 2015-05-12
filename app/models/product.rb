c4ced222751e82bb1cbe67e94710f6f5d01b4e95
Bohdan Hryshchenko <bgr11n@gmail.com>
2015-05-12 17:17:13 +0300

    Validate title length to be less than 10

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates_length_of :title, :maximum => 10, :message => "less than 10 if you don't mind"
   validates_uniqueness_of :title
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\z}i,
